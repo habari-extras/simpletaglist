2bf6abe5087c95aa67d898b471568c4efd69cb46
mikelietz <codger+git@gmail.com>
2011-11-17 20:30:50 +0000

    plugin:simpletaglist Update to echo the template, retrieve tags correctly, and have valid XML.
    git-svn-id: http://svn.habariproject.org/habari-extras/plugins/simpletaglist/trunk@4054 f4a90942-1786-4aa1-b092-57c31935ddc3

--- simpletaglist.php
+++ simpletaglist.php
@@ -1,7 +1,7 @@
 <!-- This file can be copied and modified in a theme directory -->
 <ul id="tag-links">
-<?php foreach( $theme->tag_links as $tag) { ?>
-
-	<li><a href="<?php URL::out( 'display_entries_by_tag', array( 'tag'=> $tag->slug ), false ); ?>"><?php echo $tag->tag; ?></a></li>
+<?php foreach( $theme->tag_links as $tag) {
+?>
+	<li><a href="<?php echo URL::get( 'display_entries_by_tag', array( 'tag'=> $tag->term ), false ); ?>"><?php echo $tag->term_display; ?></a></li>
 <? 	} ?>
 </ul>
--- simpletaglist.plugin.php
+++ simpletaglist.plugin.php
@@ -2,23 +2,6 @@
 class SimpleTagList extends Plugin
 {
 	/**
-	 * Add help text to plugin configuration page
-	 **/
-	public function help() 
-	{
-		$help = _t( 'To use, add <code>&lt;?php $theme->tag_links(); ?&gt;</code> to your theme where you want the list output.' );
-		return $help;
-	}
-
-	/**
-	 * Add update beacon support
-	 **/
-	public function action_update_check()
-	{
-		 Update::add( 'Simple Tag List', 'da67e437-e3b9-4d7a-bad8-5ab55b6d801b', $this->info->version );
-	}
-
-	/**
 	 * On plugin init, add the template included with this plugin to the available templates in the theme
 	 */
 	public function action_init()
@@ -30,9 +13,9 @@ class SimpleTagList extends Plugin
 	 * Add simplified tag array to the available template vars
 	 * @param Theme $theme The theme that will display the template
 	 **/
-	public function theme_tag_links( $theme )
+	public function theme_tag_links( $theme, $sortorder = 'term_display asc' )
 	{
-		$theme->tag_links = Tags::get();
+		$theme->tag_links = Tags::vocabulary()->get_tree( $sortorder );
 		return $theme->fetch( 'simpletaglist' );
 	}
 }
